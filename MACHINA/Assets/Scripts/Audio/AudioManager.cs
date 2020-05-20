6e8d49ad49c3aab70370377e4e457944cc4ac71c
白枝友紀 <saokurokirito4624@i.softbank.jp>
2020-05-21 03:49:39 +0900

    音の追加

---
+++ MACHINA/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager instance = null;
+
+	private AudioSource _source;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		_source = GetComponent<AudioSource>();
+    }
+
+	public void PlaySE(AudioClip clip)
+	{
+		_source.PlayOneShot(clip);
+	}
+
+	public void PlayOneSE(AudioClip clip)
+	{
+		if(!_source.isPlaying)
+		{
+			_source.PlayOneShot(clip);
+		}
+	}
+
+	public void PlayBGM(AudioClip clip)
+	{
+		StartCoroutine(BGMLoop(clip));
+	}
+
+	public void StopSE()
+	{
+		_source.Stop();
+	}
+
+	public void StopBGM()
+	{
+		_source.Stop();
+	}
+
+	private IEnumerator BGMLoop(AudioClip clip)
+	{
+		while(true)
+		{
+			if(!_source.isPlaying)
+			{
+
+			}
+			yield return null;
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
