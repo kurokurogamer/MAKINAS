c9a912077e9a286aea35fa77cf84e19a7a1a63df
a <a>
2020-09-14 09:53:56 +0900

    更新

---
+++ MACHINA/Assets/Scripts/HPGage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPGage : MonoBehaviour
+{
+    private float point = 10000;
+    Image _image = null;
+    PlayerSt main;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = Camera.main.GetComponent<PlayerSt>();
+        _image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_image)
+        {
+            _image.fillAmount = main.HP / 10000;
+        }
+    }
+}
